fd957dfa0957ea5d6e0bd2970c3761ea06b6b621
Gabriel Cavalcante <gscavalcante@gmail.com>
2019-08-22 18:16:58 -0300

    Revert "Fix typo"
    This reverts commit ff9fca1a175fc46a87c4d8882025453c97fee598.

--- bash/git.sh
+++ bash/git.sh
@@ -3,5 +3,5 @@ git_branch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-# There's a glitche because of the colors, so there's more info at: https://askubuntu.com/questions/81239/terminal-line-glitches
+# There's a glitch because of the colors, so there's more info at: https://askubuntu.com/questions/81239/terminal-line-glitches
 export PS1="\[\e[33;1m\]\u@\h: \[\e[31m\]\W\[\e[0m\]\$(git_branch)\$ "
